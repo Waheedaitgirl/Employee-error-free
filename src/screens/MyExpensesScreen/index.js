8fd5795f6818e90e5821b866b066efd2626e9f12
aftabameen <aftabameen@recruitbpm.com>
2022-03-14 16:28:52 +0500

    Expenses module Start

--- src/screens/MyExpensesScreen/index.js
+++ src/screens/MyExpensesScreen/index.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import { SafeAreaView,View,StyleSheet,Text} from 'react-native';
-import { commonStyles,textStyles } from '../../styles';
-import CustomButton from '../../components/Button';
-import CustomHeader from '../../components/CustomHeader';
+import { commonStyles } from '../../styles';
+
+import CustomHeader from './Header';
 import { scale } from 'react-native-size-matters';
-    const MyExpensesScreen = ({navigation}) => {
+    const AllExpenseScreen = ({navigation}) => {
         return (
             <View style={commonStyles.container} >
                 <CustomHeader 
                     show_backButton={true}
                     isdrawer={true}
                     onPress={() => navigation.openDrawer()}
-                    title={"My Expenses"}
+                    title={"All Expenses"}
                 />
-                <Text style={textStyles.title} >Welcome to</Text>
-                <Text style={textStyles.heading} >Recruit BPM</Text>
-                <View style={{height:scale(20)}} />
-                <CustomButton 
-                    onPress={() => navigation.navigate.goBack()}
-                    loading={false}
-                    text={"Save"}
-                    loadingText={"Processing"}
-                />
+               
+              
             </View>
         );
     };
 
 
-export default MyExpensesScreen;
+export default AllExpenseScreen;
